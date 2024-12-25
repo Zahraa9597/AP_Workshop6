b5d59d2b5d355fbc0e84e03ea83683e1b247cf7e
zahraa <Zahraa9597>
2024-12-25 10:12:25 -0800

    added fields, constructor, getters, abstract method to Animalclass

--- Animal.java
+++ Animal.java
@@ -1,2 +1,25 @@
-public class Animal {
+public abstract class Animal {
+    private String name;
+    private String age;
+    private String uniqueCharacteristic;
+
+    public Animal(String name, String age, String uniqueCharacteristic) {
+        this.name = name;
+        this.age = age;
+        this.uniqueCharacteristic = uniqueCharacteristic;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+    public String getAge() {
+
+        return age;
+    }
+    public String getUniqueCharacteristic() {
+        return this.uniqueCharacteristic;
+    }
+    public abstract void show();
+
 }
