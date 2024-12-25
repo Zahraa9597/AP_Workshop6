86c987b8ab9d74c853fd73c461906c21a923593d
zahraa <Zahraa9597>
2024-12-25 10:22:55 -0800

    added constructor and overrided show method

--- Eagle.java
+++ Eagle.java
@@ -1,2 +1,10 @@
 public class Eagle extends Bird implements Hunter{
+
+    public Eagle(String name, String age,String uniqueCharacteristic, String heightOfFly) {
+        super(name, age,uniqueCharacteristic, heightOfFly);
+    }
+    @Override public void show() {
+        System.out.println("Name: " + getName() + ", Age: " + getAge() + ", Flight Height: " + getHeightOfFly() + ", Special Feature: " + getUniqueCharacteristic());
+    }
+
 }
--- Parrot.java
+++ Parrot.java
@@ -1,2 +1,10 @@
-public class Parrot extends Bird implements Prey{
+public class Parrot extends Bird implements Prey {
+
+    public Parrot(String name, String age, String uniqueCharacteristic, String heightOfFly) {
+        super(name, age, uniqueCharacteristic, heightOfFly);
+    }
+    @Override public void show() {
+        System.out.println("Name: " + getName() + ", Age: " + getAge() + ", Flight Height: " + getHeightOfFly() + ", Special Feature: " + getUniqueCharacteristic());
+    }
 }
+
