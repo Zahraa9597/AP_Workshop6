7cb91ea89e5b65f23d4cd0c49c1379b88321ba38
zahraa <Zahraa9597>
2024-12-26 09:49:44 -0800

    created objects, added to an ArrayList, call hund method

--- Main.java
+++ Main.java
@@ -1,15 +1,28 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Eagle eagle = new Eagle("Eagle", 5, "Sharp eyesight", 100);
+        Parrot parrot = new Parrot("Parrot", 3, "Colorful feathers", 30);
+        Cheetah cheetah = new Cheetah("Cheetah", 4, "The fastest", 120);
+        Giraffe giraffe = new Giraffe("Giraffe", 6, "Long neck", 15);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        ArrayList<Animal> animals = new ArrayList<>();
+
+        animals.add(eagle);
+        animals.add(parrot);
+        animals.add(cheetah);
+        animals.add(giraffe);
+
+        for (Animal animal : animals) {
+            animal.show();
         }
+        cheetah.hunt(parrot);
+        cheetah.hunt(giraffe);
+        eagle.hunt(parrot);
+        eagle.hunt(giraffe);
     }
+
 }
