70744899ccb03f7dfb3321db8fc9abee93547d7b
zahraa <Zahraa9597>
2024-12-26 09:38:55 -0800

    created overrided hunt method

--- Cheetah.java
+++ Cheetah.java
@@ -1,10 +1,15 @@
 public class Cheetah extends Mammal implements Hunter{
 
-    public Cheetah(String name, String age, String uniqueCharacteristic, int speed) {
+    public Cheetah(String name, int age, String uniqueCharacteristic, int speed) {
         super(name, age, uniqueCharacteristic, speed);
     }
     @Override public void show() {
         System.out.println("Name: " + getName() + ", Age: " + getAge() + ", Speed: " + getSpeed() + ", Special Feature: " + getUniqueCharacteristic());
+    }
+
+    @Override
+    public void hunt(Prey prey) {
+        System.out.println(getName() + " hunted " + prey.getName());
     }
 }
 
--- Eagle.java
+++ Eagle.java
@@ -1,10 +1,18 @@
-public class Eagle extends Bird implements Hunter{
+public class Eagle extends Bird implements Hunter {
 
-    public Eagle(String name, String age,String uniqueCharacteristic, String heightOfFly) {
-        super(name, age,uniqueCharacteristic, heightOfFly);
+    public Eagle(String name, int age, String uniqueCharacteristic, int heightOfFly) {
+        super(name, age, uniqueCharacteristic, heightOfFly);
     }
-    @Override public void show() {
+
+    @Override
+    public void show() {
         System.out.println("Name: " + getName() + ", Age: " + getAge() + ", Flight Height: " + getHeightOfFly() + ", Special Feature: " + getUniqueCharacteristic());
     }
+
+    @Override
+    public void hunt(Prey prey) {
+        System.out.println(getName() + " hunted " + prey.getName());
+    }
+
 
 }
