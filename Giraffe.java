15e41351cddb38cca966aeb43df60f7b2a6b4e22
zahraa <Zahraa9597>
2024-12-26 09:38:09 -0800

    created overrided Prey method

--- Giraffe.java
+++ Giraffe.java
@@ -1,8 +1,14 @@
 public class Giraffe extends Mammal implements Prey{
-    public Giraffe(String name, String age, String uniqueCharacteristic, int Speed ) {
+    public Giraffe(String name, int age, String uniqueCharacteristic, int Speed ) {
         super(name, age, uniqueCharacteristic, Speed);
     }
-    @Override public void show() {
+    @Override
+    public void show() {
         System.out.println("Name: " + getName() + ", Age: " + getAge() + ", Speed: " + getSpeed() + ", Special Feature: " + getUniqueCharacteristic());
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
     }
 }
--- Parrot.java
+++ Parrot.java
@@ -1,10 +1,16 @@
 public class Parrot extends Bird implements Prey {
 
-    public Parrot(String name, String age, String uniqueCharacteristic, String heightOfFly) {
+    public Parrot(String name, int age, String uniqueCharacteristic, int heightOfFly) {
         super(name, age, uniqueCharacteristic, heightOfFly);
     }
     @Override public void show() {
         System.out.println("Name: " + getName() + ", Age: " + getAge() + ", Flight Height: " + getHeightOfFly() + ", Special Feature: " + getUniqueCharacteristic());
     }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
 }
+
 
